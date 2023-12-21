23bc24189f766bb8cbf240a04fe8e9021c371d8a
Aaron Maslen <aaron.maslen@gmail.com>
2023-12-21 11:47:11 +1100

    Add new patch: Fix for Difficulty setting DR Reduction not ignoring friendly fire

---
+++ Patches/DRDifficultyIgnoreAllies.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+using HarmonyLib;
+
+using Kingmaker.PubSubSystem.Core;
+using Kingmaker.RuleSystem;
+using Kingmaker.RuleSystem.Rules.Damage;
+
+using MicroUtils.Transpiler;
+
+namespace MicroPatches.Patches
+{
+    [MicroPatch("Do not apply Damage Reduction modifiers from difficulty to friendly fire")]
+    [HarmonyPatch(typeof(RuleRollDamage), nameof(RuleRollDamage.ApplyDifficultyModifiers))]
+    [HarmonyPatchCategory(Main.Category.Experimental)]
+    internal static class DRDifficultyIgnoreAllies
+    {
+        [HarmonyTranspiler]
+        static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+        {
+            var match = instructions.FindInstructionsIndexed(
+            [
+                ci => ci.Calls(AccessTools.PropertyGetter(typeof(IMechanicEntity), nameof(IMechanicEntity.IsPlayerFaction))),
+                ci => ci.opcode == OpCodes.Brfalse_S || ci.opcode == OpCodes.Brfalse
+            ]).ToArray();
+
+            if (match.Length != 2)
+            {
+                throw new Exception("Could not find instructions to patch");
+            }
+
+            var iList = instructions.ToList();
+
+            var branchTarget = (Label)match[1].instruction.operand;
+
+            iList.InsertRange(match[1].index + 1,
+            [
+                new CodeInstruction(OpCodes.Ldarg_0),
+                new CodeInstruction(OpCodes.Ldfld, AccessTools.Field(typeof(RulebookTargetEvent), nameof(RulebookTargetEvent.Target))),
+                new CodeInstruction(OpCodes.Callvirt, AccessTools.PropertyGetter(typeof(IMechanicEntity), nameof(IMechanicEntity.IsPlayerFaction))),
+                new CodeInstruction(OpCodes.Brtrue_S, branchTarget)
+            ]);
+
+            return iList;
+        }
+    }
+}
